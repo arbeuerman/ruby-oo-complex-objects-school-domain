404adc29b8a6631a06307a9a4f730b89ac84babf
Alexandra <arbeuerman@gmail.com>
2021-03-16 14:57:40 -0500

    created school class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
+require 'pry'
+
 # code here!
+
+class School
+
+    attr_reader :roster
+
+    def initialize name 
+        @roster = {}
+        @name = name
+    end
+
+    def add_student(student_name, grade_level)
+        if @roster.has_key? grade_level
+            @roster[grade_level] << student_name
+        else
+            @roster[grade_level] = []
+            @roster[grade_level] << student_name
+        end 
+    end
+
+    def grade grade_level
+        @roster.fetch(grade_level)        
+    end 
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end 
+    end 
+    
+end 
+
+#west = School.new #("Madison West High School")
+#binding.pry 
